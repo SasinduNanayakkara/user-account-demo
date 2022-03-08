d741248fc7c631c7d2e18046e34fe438dbb80f84
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-03-08 22:09:25 +0530

    implement routes

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const {addUser, userLogin, getUsers} = require("../controllers/user.controller");
+const {addUser, userLogin, getUsers, updateUser, deleteUser} = require("../controllers/user.controller");
 const User = require("../models/user");
 const verifyToken = require("../verifyToken");
 
@@ -7,5 +7,7 @@ const verifyToken = require("../verifyToken");
 router.post("/register", addUser);
 router.post("/login", userLogin);
 router.get("/users", verifyToken, getUsers); //private route
+router.put("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", deleteUser);
 
-module.exports = router
+module.exports = router;
